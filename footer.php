711e6f09d2eb78974d32735bf1cabb8295529c21
juinerS <38709520+juinerS@users.noreply.github.com>
2018-05-04 12:13:34 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,48 +1,140 @@
 
 <!--Footer-->
-<footer class="page-footer font-small bg-pagina pt-4 mt-4">
+<footer class="page-footer font-small unique-color-dark pt-0">
+
+    <div style="background-color: #6351ce;">
+        <div class="container">
+            <!--Grid row-->
+            <div class="row py-4 d-flex align-items-center">
+
+                <!--Grid column-->
+                <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+                    <h6 class="mb-0 white-text">¡Conéctese con nosotros en las redes sociales!</h6>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-6 col-lg-7 text-center text-md-right">
+                    <!--Facebook-->
+                    <a class="fb-ic ml-0">
+                        <i class="fa fa-facebook white-text mr-lg-4"> </i>
+                    </a>
+                    <!--Twitter-->
+                    <a class="tw-ic">
+                        <i class="fa fa-twitter white-text mr-lg-4"> </i>
+                    </a>
+                    <!--Google +-->
+                    <a class="gplus-ic">
+                        <i class="fa fa-google-plus white-text mr-lg-4"> </i>
+                    </a>
+                    <!--Linkedin-->
+                    <a class="li-ic">
+                        <i class="fa fa-linkedin white-text mr-lg-4"> </i>
+                    </a>
+                    <!--Instagram-->
+                    <a class="ins-ic">
+                        <i class="fa fa-instagram white-text mr-lg-4"> </i>
+                    </a>
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+        </div>
+    </div>
 
     <!--Footer Links-->
-    <div class="container-fluid text-center text-md-left">
-        <div class="row">
+    <div class="container mt-5 mb-4 text-center text-md-left">
+        <div class="row mt-3">
 
             <!--First column-->
-            <div class="col-md-6">
-                <h5 class="text-uppercase">Turaonline.co</h5>
-                <p>porque buscar trabajo nunca fue tan facil.</p>
+            <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+                <h6 class="text-uppercase font-weight-bold">
+                    <strong>TuraOnline</strong>
+                </h6>
+                <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
+                    amet, consectetur adipisicing elit.</p>
             </div>
             <!--/.First column-->
 
             <!--Second column-->
-            <div class="col-md-6">
-                <h5 class="text-uppercase">Links</h5>
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 2</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 4</a>
-                    </li>
-                </ul>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 class="text-uppercase font-weight-bold">
+                    <strong>Productos</strong>
+                </h6>
+                <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>
+                    <a href="#!">MDBootstrap</a>
+                </p>
+                <p>
+                    <a href="#!">MDWordPress</a>
+                </p>
+                <p>
+                    <a href="#!">BrandFlow</a>
+                </p>
+                <p>
+                    <a href="#!">Bootstrap Angular</a>
+                </p>
             </div>
             <!--/.Second column-->
-            
+
+            <!--Third column-->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 class="text-uppercase font-weight-bold">
+                    <strong>Enlaces útiles</strong>
+                </h6>
+                <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>
+                    <a href="#!">Su cuenta</a>
+                </p>
+                <p>
+                    <a href="#!">Empresas</a>
+                </p>
+                <p>
+                    <a href="#!">Ofertas de empleo</a>
+                </p>
+                <p>
+                    <a href="#!">Publicar HV</a>
+                </p>
+                <p>
+                    <a href="#!">Ayuda</a>
+                </p>
+            </div>
+            <!--/.Third column-->
+
+            <!--Fourth column-->
+            <div class="col-md-4 col-lg-3 col-xl-3">
+                <h6 class="text-uppercase font-weight-bold">
+                    <strong>Contact</strong>
+                </h6>
+                <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>
+                    <i class="fa fa-home mr-3"></i> Buenaventrua, Valle del Cauca, CO</p>
+                <p>
+                    <i class="fa fa-envelope mr-3"></i> info@example.com</p>
+                <p>
+                    <i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+                <p>
+                    <i class="fa fa-print mr-3"></i> + 01 234 567 89</p>
+            </div>
+            <!--/.Fourth column-->
+
         </div>
     </div>
     <!--/.Footer Links-->
 
-    <!--Copyright-->
-    <div class="footer-copyright text-center">
+    <!-- Copyright-->
+    <div class="footer-copyright py-3 text">
         © 2018 Copyright:
-        <a href="#"> Turaonline.co</a>
+        <a href="index.php">
+            <strong> TuraOnline.com</strong>
+        </a>
     </div>
-    <!--/.Copyright-->
+    <!--/.Copyright -->
+
+</footer>
+<!--/.Footer-->
 
     <!--Scripts-->
     <script type="text/javascript" src="/vendor/jquery/jquery.min.js"></script>
@@ -54,5 +146,3 @@
     <script type="text/javascript" src="/vendor/bootstrap-datepicker/locales/bootstrap-datepicker.es.min.js"></script>
     <script type="text/javascript" src="/js/main.js"></script>
     <!--/.Scripts-->
-</footer>
-<!--/.Footer-->
